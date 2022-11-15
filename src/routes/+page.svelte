590f3f1d729c4b8db28f598447e3205e19223623
chrisgabs <cgabayquen@gmail.com>
2022-11-15 19:42:41 +0800

    feat: initial layout of application

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,42 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+
+</script>
+
+<div class="flex bg-slate-500 h-screen m-0 align-center">
+
+    <div class="flex-col m-auto my-auto">
+
+        <div class="accounts outline outline-1 p-2">
+            <div class="account bg-slate-400 w-fit">
+                <div class="account_name">Account 1</div>
+                <div class="account_balance">Balance: $100</div>
+            </div>
+        </div>
+
+        <div class="add-item flex justify-between space-x-4 outline outline-1 p-2 mt-2">
+            <select name="type" id="type" class="">
+                <option value="Food">Text</option>
+                <option value="Trasnportation">Image</option>
+                <option value="Others">Video</option>
+            </select>
+            <select name="account" id="account" class="">
+                <option value="Gcash">Gcash</option>
+                <option value="Cash">Cash</option>
+                <option value="Bank">Bank</option>
+            </select>
+            <input type="text" name="amount" id="amount" class="" placeholder="Amount">
+            <input type="button" value="Add" class="add-button outline outline-1">
+        </div>
+        
+        <div class="records-container mt-2">
+            <div class="record-item flex justify-between items-center outline outline-1 p-2">
+                <div class="picture w-8 h-8 bg-white"></div>
+                <span>Type</span>
+                <span autocomplete="off">Amount</span>
+                <span>Date</span>
+            </div>
+        </div>
+
+    </div>
+
+</div>
