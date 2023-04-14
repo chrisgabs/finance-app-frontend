8f480e3e8eb542e7817a927d8175c0b649de8682
Christian Bayquen <cgabayquen@gmail.com>
2023-04-14 16:38:55 +0800

    feat: login/signup form validation

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,18 +4,63 @@
 
     let loginContainer: HTMLElement;
     let login:boolean = true;
+    let invalidCreds = false;
+    let passwordMatching = true;
+    let emailValid = true;
+    let validPassword = true;
     
     const scrollToLoginContainer = () => {
         loginContainer.scrollIntoView({behavior: "smooth", block:"end"});
     }
 
+    const resetValidators = () => {
+        invalidCreds = false;
+        passwordMatching = true;
+        emailValid = true;
+        validPassword = true;
+    }
+
     const loginEnhancement: SubmitFunction = ({ form, data, action, cancel }) => {
 
-        // TODO: Client side form validation here
+        resetValidators()
+        const email = data.get('email') as string;
+        const password = data.get('password') as string;
+
+        if (action.search == "?/register") {
+            const confirmPassword = data.get('confirm-password');
+    
+            // Check for valid email format
+            const emailRegex = /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$/;
+            console.log(emailRegex.test(email))
+            if (!emailRegex.test(email)) {
+                emailValid = false;
+                return cancel();
+            }
+    
+            // Check if password and confirm password match
+            if (password.length < 6) {
+                validPassword = false;
+                return cancel()
+            }
+            if (password !== confirmPassword) {
+                passwordMatching = false;
+                return cancel();
+            }
+        } else if (action.search == "?/login") {
+            console.log("login checking")
+            if (email == "" || password == "") {
+                invalidCreds = true;
+                return cancel();
+            }
+        }
 
         return async ({ result, update }) => {
             switch (result.type) {
+                case 'success':
+                    invalidCreds = false;
+                    break;
                 case 'failure':
+                    invalidCreds = true;
                     console.log(result.data!.error)
                     break;
                 default:
@@ -49,15 +94,30 @@
 
         <!-- login form -->
 		<form method="POST" class="bg-white p-10 outline outline-1 rounded-md outline-neutral/25" use:enhance={loginEnhancement}>
-			<h1 class="text-gray-800 font-bold text-2xl text-center mb-6">
+			<h1 class="text-gray-800 font-bold text-2xl text-center mb-4">
                 {login ? 'Login' : 'Register'}
             </h1>
-			<!-- <p class="text-sm font-normal text-gray-600 mb-5 text-center">Login</p> -->
+            {#if invalidCreds}
+                <p class="text-sm font-bold text-error text-center">Wrong Credentials</p>
+                <p class="text-sm font-normal text-error mb-2 text-center">invalid email or password</p>
+            {/if}
+            {#if !passwordMatching || !emailValid || !validPassword}
+                <p class="text-sm font-bold text-error text-center">Invalid Input</p>
 
+                {#if !passwordMatching}
+                    <p class="text-sm font-normal text-error text-center">passwords should match</p>
+                {/if}
+                {#if !emailValid}
+                    <p class="text-sm font-normal text-error text-center">email must be valid</p>
+                {/if}
+                {#if !validPassword}
+                    <p class="text-sm font-normal text-error text-center">password must be at least 6 characters long</p>
+                {/if}
+            {/if}
             <!-- <div class="divider my-1"></div> -->
 
             <!-- email -->
-            <div class="flex items-center border-[1px] border-current/90 py-2 px-3 rounded-2xl mb-4">
+            <div class="flex items-center border-[1px] border-current/90 py-2 px-3 rounded-2xl mb-4 mt-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
                     viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -96,7 +156,7 @@
                 {login ? 'Login' : 'Create Account'}
             </button>
             <div class="flex justify-between">
-                <button on:click={(e) => {e.preventDefault(); login = !login}} class="text-sm ml-2 hover:text-blue-500 cursor-pointer">
+                <button on:click={(e) => {e.preventDefault(); login = !login; resetValidators()}} class="text-sm ml-2 hover:text-blue-500 cursor-pointer">
                     {login ? 'Register' : 'Login'}
                 </button>
                 <button disabled class="text-sm ml-2 text-gray-400">Forgot Password?</button>
